be176effde9c8efce9187f8b0d72a7595bec3eda
Tanuj Singh <tanujsingh@Tanujs-MacBook-Air.local>
2020-08-04 06:18:33 +0530

    updates

--- add/add.ts
+++ add/add.ts
@@ -2,14 +2,8 @@ function add(param1: number, param2: number): number {
     return param1 + param2
 }
 
-function add2(...param1: number[]): number {
-    let total = 0;
-    
-    param1.forEach((num) =>
-        total += num
-    )
-    
-    return total
+function add2(...param1: number[]): number {    // Rest Operator if you don't know the number of inputs
+    return param1.reduce((total, num) => total + num)
 }
 
 console.log(add(1, 2));
