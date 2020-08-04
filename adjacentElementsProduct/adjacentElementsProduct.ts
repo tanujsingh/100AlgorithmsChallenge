cb5d02ccbfb28bfc123b0c0ee96ff09a54da9006
Tanuj Singh <tanujsingh@Tanujs-MacBook-Air.local>
2020-08-05 01:53:43 +0530

    Done

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,19 @@
 function adjacentElementsProduct(inputArray: number[]): number {
+    let biggerProduct = inputArray[0] * inputArray[1]   //For real case scenario, don't use 0 as the base.
+    for (let i = 1; i < inputArray.length - 1; i++) {
+        // Solution 1
+        // if (inputArray[i-1] * inputArray[i] > inputArray[i] * inputArray[i+1]) {
+        //     biggerProduct = inputArray[i-1] * inputArray[i]
+        // } else {
+        //     biggerProduct = inputArray[i] * inputArray[i+1]
+        // }
 
+        // Solution 2
+        const product = inputArray[i] * inputArray[i+1]
+
+        biggerProduct = product > biggerProduct ? product : biggerProduct
+    }
+    return biggerProduct
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
