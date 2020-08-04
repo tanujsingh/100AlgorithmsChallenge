6b460d4cc8c553a86b5909530116f5862cc76fac
Tanuj Singh <tanujsingh@Tanujs-MacBook-Air.local>
2020-08-05 01:35:55 +0530

    Done

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,23 @@
 function addTwoDigits(n: any): number {
+    // const arr = n.toString().split('')
+    // Solution 1
+    // let sum = 0
+    
+    // arr.forEach((num: string) => {
+    //     sum += parseInt(num)
+    // })
+    // return sum
 
+    // Solution 2
+//   return arr.reduce((a: String, b: String) => {
+//     return parseInt(a) + parseInt(b)
+// })
+
+    //Solution 3
+    // return parseInt(arr[0]) + parseInt(arr[1])
+
+    //Solution 4
+    return n % 10 + Math.floor(n / 10)
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
