3084364c1223dd848e98a09d39235f7a9a423167
Tanuj Singh <tanujsingh@Tanujs-MacBook-Air.local>
2020-08-07 17:43:34 +0530

    Done

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,16 @@
 function allLongestStrings(inputArray: string[]): string[] {
+    let outputArray = []
+    let longestLength = 0
 
+   inputArray.forEach((word: string) => {longestLength = longestLength > word.length ? longestLength : word.length})
+
+   inputArray.forEach((word: string) {
+       if (word.length === longestLength) {
+           outputArray.push(word)
+       }
+   })
+
+    return outputArray
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
