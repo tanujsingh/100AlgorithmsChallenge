a29de4b78a3f96884d42ef4f04e42a569b9ae936
Tanuj Singh <tanujsingh@Tanujs-MacBook-Air.local>
2020-08-08 00:32:33 +0530

    Done

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,14 @@
 function alphabeticShift(inputString: string): string {
+    let outputString = ''
+    let newAlphabetCode = 0
+    let alphabetArray = inputString.split('')
+    
+    alphabetArray.forEach((alphabet: string) => {
+        newAlphabetCode = alphabet !== 'z' ? alphabet.charCodeAt(0) + 1 : alphabet.charCodeAt(0) - 25
+        outputString += String.fromCharCode(newAlphabetCode)
+    })
 
+    return outputString 
 }
 
 console.log(alphabeticShift('crazy'));
