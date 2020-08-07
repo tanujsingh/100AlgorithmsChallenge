1fbeaa7c0d6e7a2609a7cb230b9874cfde05eb99
Tanuj Singh <tanujsingh@Tanujs-MacBook-Air.local>
2020-08-08 02:04:38 +0530

    Done

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,17 @@
 function alternatingSums(a: number[]): number[] {
+    let evenWeight = 0
+    let oddWeight = 0
 
+    a.forEach((num, index) => {
+        if (index % 2 === 0) {
+            oddWeight += num 
+        } else {
+            evenWeight += num
+        }
+    })
+
+    return [oddWeight, evenWeight]
 }
+    
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
