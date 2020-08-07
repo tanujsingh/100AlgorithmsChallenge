e26a314e1bb045bdcfcbb9c5507771ba558feb94
Tanuj Singh <tanujsingh@Tanujs-MacBook-Air.local>
2020-08-08 01:06:36 +0530

    Done

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,9 +1,24 @@
 function alphabetSubsequence(s: string): boolean {
+    let inSequence = false
+    let individualChar: string[] = s.split('')
 
+    for (let i = 1; i < individualChar.length; i++) {
+        inSequence = individualChar[i].charCodeAt(0) > individualChar[i-1].charCodeAt(0) ? true : false
+        
+        if (inSequence === false) {
+            break
+        }
+    }
+
+    return inSequence 
 }
 
-console.log(alphabetSubsequence('zab'))
-console.log(alphabetSubsequence('effg'))
-console.log(alphabetSubsequence('cdce'))
-console.log(alphabetSubsequence('ace'))
-console.log(alphabetSubsequence('bxz'))
+console.log(alphabetSubsequence('zab')) // false
+console.log(alphabetSubsequence('effg')) // false
+console.log(alphabetSubsequence('cdce')) // false
+console.log(alphabetSubsequence('ace')) // true
+console.log(alphabetSubsequence('bxz')) // true
+
+// Set() -- It only takes different elements and not duplicates.
+// E.g. -- new Set(charValues) will only takes Characters with different code.
+
