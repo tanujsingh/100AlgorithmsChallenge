f6f3d263cba3055aab2332b3ef70a3aee53f4f1a
Tanuj Singh <tanujsingh@Tanujs-MacBook-Air.local>
2020-08-08 21:01:54 +0530

    Done

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,17 @@
 function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+    let bothEquallyStrong = false
+    
+    const yourStrongestArm = yourLeft > yourRight ? yourLeft : yourRight
+    const yourWeakestArm = yourLeft < yourRight ? yourLeft : yourRight
+    
+    const friendsStrongestArm = friendsLeft > friendsRight ? friendsLeft : friendsRight
+    const friendsWeakestArm = friendsLeft < friendsRight ? friendsLeft : friendsRight
+    
+    bothEquallyStrong = yourStrongestArm === friendsStrongestArm && yourWeakestArm === friendsWeakestArm ? true : false
 
+    return bothEquallyStrong
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
