5dcd7b5fc5d5837fddf8628fbe43cb1de7ab0ce6
Tanuj Singh <tanujsingh@Tanujs-MacBook-Air.local>
2020-08-09 16:33:49 +0530

    Done

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,14 @@
 function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+    let maxDifference = Math.abs(inputArray[1] - inputArray[0])
 
+    for (let i = 1; i < inputArray.length; i++) {        
+        let difference = Math.abs(inputArray[i - 1] - inputArray[i])
+        if (difference > maxDifference) {
+            maxDifference = difference
+        }
+    }
+    
+    return maxDifference
 }
 
-console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0, 6, 8, 15]));
