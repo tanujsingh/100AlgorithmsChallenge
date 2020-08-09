a7eee4c9fbdd20427b9ef6a55d4629a98e64096b
Tanuj Singh <tanujsingh@Tanujs-MacBook-Air.local>
2020-08-09 15:54:12 +0530

    Done

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,37 @@
 function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
+    let maxSum = 0
+    let sum = 0
 
+    // Method 1
+    // for (let i = 0; i < inputArray.length; i++) {
+    //     let sum = 0
+
+    //     for (let j = i; j < i + k; j++) {
+    //         sum += inputArray[j]
+    //     }
+
+    //     if (sum > maxSum) {
+    //         maxSum = sum
+    //     }
+    // }
+
+    // Method 2 (better)
+    for (let i = 0; i < k; i++) {
+        sum += inputArray[i]
+    }
+
+    maxSum = sum
+
+    for (let i = k; i < inputArray.length; i++) {
+        sum -= inputArray[i - k]
+        sum += inputArray[i]
+
+        if (sum > maxSum) {
+            maxSum = sum
+        }
+    }
+
+    return maxSum
 }
 
 console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
