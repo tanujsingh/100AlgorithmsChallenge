a714b691d291f5a8b9955640ec2176804bba0ef8
Tanuj Singh <tanujsingh@Tanujs-MacBook-Air.local>
2020-08-09 11:40:00 +0530

    Done

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,5 +1,17 @@
 function arrayConversion(inputArray: number[]): number {
+    let sumProducts: number[] = []
 
+    for (let i = 0; i <= inputArray.length; i++) {
+        
+        for (let j = 0; j < inputArray.length; j += 2) {
+        i % 2 === 0 ? sumProducts.push(inputArray[j] + inputArray[j + 1]) : sumProducts.push(inputArray[j] * inputArray[j + 1])
+        }
+        
+        inputArray = sumProducts
+        sumProducts = []
+    }
+    return inputArray[0]
 }
 
-console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
+console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8])); // 186
+console.log(arrayConversion([1, 2, 3, 4])); // 21
