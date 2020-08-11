8a78476c6d2a18d1515585bffba3b62da083f25d
Tanuj Singh <tanujsingh@Tanujs-MacBook-Air.local>
2020-08-12 04:41:06 +0530

    Done

--- bishopAndPawn/bishopAndPawn.ts
+++ bishopAndPawn/bishopAndPawn.ts
@@ -1,5 +1,25 @@
 function bishopAndPawn(bishop: string, pawn: string): boolean {
+    const chessBoard = {
+        "a": 1,
+        "b": 2,
+        "c": 3,
+        "d": 4,
+        "e": 5,
+        "f": 6,
+        "g": 7,
+        "h": 8
+    }
 
+    const bishopX = chessBoard[bishop[0]]
+    const bishopY = parseInt(bishop[1])
+    const pawnX = chessBoard[pawn[0]]
+    const pawnY = parseInt(pawn[1])
+
+    if (bishopX + bishopY === pawnY + pawnX || bishopX + pawnY === pawnX + bishopY) {
+        return true
+    }
+
+    return false
 }
 
 console.log(bishopAndPawn('a1', 'c3'));
