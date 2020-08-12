0629bbe53dcf58dfae4b9138fa366709ab147cc8
Tanuj Singh <tanujsingh@Tanujs-MacBook-Air.local>
2020-08-12 06:58:17 +0530

    Done

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,5 +1,6 @@
 function characterParity(symbol: string): string {
-   
+    
+    return isNaN(parseInt(symbol)) ? 'Not a Digit' : parseInt(symbol) % 2 === 0 ? 'Even' : 'Odd'
 }
 
 console.log(characterParity('5'))
