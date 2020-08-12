02409cd67a33be96f05a2f0106ea194d09e80263
Tanuj Singh <tanujsingh@Tanujs-MacBook-Air.local>
2020-08-12 06:38:49 +0530

    Done

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -2,11 +2,7 @@ function isCaseInsensitivePalindrome(inputString: string): boolean {
     inputString = inputString.toLowerCase()
     const palindrome = inputString.split('').reverse().join('')
 
-    if (palindrome === inputString) {
-        return true
-    }
-
-    return false
+    return palindrome === inputString
 
 }
 
