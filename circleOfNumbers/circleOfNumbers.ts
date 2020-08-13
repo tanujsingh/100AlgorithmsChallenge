c9141a43eacea20d8ca14583f8235a81c1a04a71
Tanuj Singh <tanujsingh@Tanujs-MacBook-Air.local>
2020-08-14 03:33:17 +0530

    Done

--- circleOfNumbers/circleOfNumbers.ts
+++ circleOfNumbers/circleOfNumbers.ts
@@ -1,5 +1,10 @@
 function circleOfNumbers(n: number, firstNumber: number): number {
 
+    if (firstNumber < n / 2) {
+        return firstNumber + n / 2 
+    } else {
+        return firstNumber - n / 2
+    }
 }
 
 console.log(circleOfNumbers(10, 2));
