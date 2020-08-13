f25c2f0c5bd6cae0fd73a9402934db90494bc6b4
Tanuj Singh <tanujsingh@Tanujs-MacBook-Air.local>
2020-08-14 02:41:11 +0530

    Done

--- chessBoardCellColor/chessBoardCellColor.ts
+++ chessBoardCellColor/chessBoardCellColor.ts
@@ -1,4 +1,21 @@
 function chessBoardCellColor(cell1: string, cell2: string): boolean {
+    const board = {
+        "A": 1,
+        "B": 2,
+        "C": 3,
+        "D": 4,
+        "E": 5,
+        "F": 6,
+        "G": 7,
+        "H": 8 
+    }
+    
+    const cell1X = board[cell1[0]]
+    const cell1Y = parseInt(cell1[1])
+    const cell2X = board[cell2[0]]
+    const cell2Y = parseInt(cell2[1])
+
+    return (cell1X - cell1Y) % 2 === 0 && (cell2X - cell2Y) % 2 === 0
 
 }
 
